02af7502520ed3088c1dc4d599d91cf67f69c91e
Kola Kolade <kolakolade@gmail.com>
2023-03-10 15:35:51 +0100

    implement app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Pokedex from './components/Pokedex';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Pokedex />
+    </div>
+  );
 }
 
 export default App;
